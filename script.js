5e86c27e0cd96afc062992114f69c5a76a3562d8
David Aylward <Davidayl14@outlook.com>
2022-03-02 20:07:33 -0500

    added js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+var questionsArray = [
+  {
+    text: "What is 1 + 1",
+    choices: [1, 2, 11, 44],
+    answer: "2",
+  },
+  {
+    text: "What is 10/2",
+    choices: [5, 6, 17, 42],
+    answer: "5",
+  },
+  {
+    text: "How many eggs in a dozen",
+    choices: [6, 2, 42, 12],
+    answer: "12",
+  },
+];
+
+var currentIndex = 0;
+
+console.log(questionsArray[1].text);
+
 // setting variables for the quiz
 
 var beginEl = document.querySelector("#start");
 var mainEl = document.querySelector("#main");
+
 var endEl = document.querySelector("#end");
 
 var startBtn = document.querySelector("#startbtn");
@@ -15,9 +38,11 @@ function beginSection() {
   endEl.style.display = "none";
 }
 function mainSection() {
-  beginEl.style.display = "none";
   mainEl.style.display = "block";
+  beginEl.style.display = "none";
   endEl.style.display = "none";
+
+  displayQuestion();
 }
 function endSection() {
   beginEl.style.display = "none";
@@ -29,6 +54,49 @@ function sendMessage() {
   timeEl.textContent = "Times Up";
 }
 
+function nextQuestion() {
+  currentIndex++;
+  console.log(currentIndex);
+
+  // reset the container
+  mainEl.innerHTML = "";
+
+  displayQuestion();
+}
+
+function displayQuestion() {
+  // <h3>Questions</h3>
+  // <button>2</button>
+  // <button>6</button>
+  // <button>11</button>
+  // <button>44</button>
+  var questionh3 = document.createElement("h3");
+
+  questionh3.textContent = questionsArray[currentIndex].text;
+  mainEl.append(questionh3);
+
+  // var answer1 = document.createElement("button");
+  // answer1.textContent = questionsArray[currentIndex].choices[0];
+
+  // var answer2 = document.createElement("button");
+  // answer2.textContent = questionsArray[currentIndex].choices[1];
+
+  // var answer3 = document.createElement("button");
+  // answer3.textContent = questionsArray[currentIndex].choices[2];
+
+  // var answer4 = document.createElement("button");
+  // answer4.textContent = questionsArray[currentIndex].choices[3];
+
+  for (i = 0; i < questionsArray[currentIndex].choices.length; i++) {
+    var answerEl = document.createElement("button");
+    answerEl.textContent = questionsArray[currentIndex].choices[i];
+
+    answerEl.addEventListener("click", nextQuestion);
+
+    mainEl.append(answerEl);
+  }
+}
+
 // setting up button to start game
 
 startBtn.addEventListener("click", mainSection);
@@ -36,9 +104,6 @@ startBtn.addEventListener("click", setTime);
 
 // Selects element by class
 var timeEl = document.querySelector(".time");
-
-// Selects element by id
-var mainEl = document.getElementById("timer");
 
 var secondsLeft = 60;
 
